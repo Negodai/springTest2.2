4c68b580c0940cb0ded0bf1d6ea8c29e72ba5824
Negodai <i.i.pro@yandex.ru>
2024-12-07 16:19:35 +0300

    init

---
+++ src/main/java/ru/springTest/Main.java
@@ -0,0 +1,20 @@
+package ru.springTest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.springTest.config.DrawConfig;
+import ru.springTest.shapeDraw.Scene;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DrawConfig.class);
+
+        context.getBean(Scene.class).draw();
+
+        for(String name : context.getBeanDefinitionNames())
+            System.out.println(name);
+
+    }
+}
---
+++ src/main/java/ru/springTest/shapeDraw/Scene.java
@@ -0,0 +1,25 @@
+package ru.springTest.shapeDraw;
+
+import java.util.List;
+
+public class Scene {
+    List<Shape> objects;
+
+    public Scene() {}
+
+    public Scene(List<Shape> objects) {
+        this.objects = objects;
+    }
+
+    public void draw() {
+        objects.forEach(Shape::draw);
+    }
+
+    public List<Shape> getObjects() {
+        return objects;
+    }
+
+    public void setObjects(List<Shape> objects) {
+        this.objects = objects;
+    }
+}
